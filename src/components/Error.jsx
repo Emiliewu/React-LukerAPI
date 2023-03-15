7583de36aec82d71b0f248863a3d7763e62674ce
Emilie <emiliewu@163.com>
2023-03-15 10:43:41 -0700

    update

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import img from '../images/Ben_Kenobi.png';
+import { Link } from "react-router-dom";
 
 function Error() {
   return (
     <div>
+      <h1>oops...</h1>
       <img src={img} alt="oops...there is something wrong" />
+      <Link to="/">try again</Link>
     </div>
   )
 }
